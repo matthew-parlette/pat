ca59616e8bb3319946e8ef30ae87698fa6fd5df3
Matt Parlette <matthew.parlette@gmail.com>
2014-10-17 21:50:49 -0400

    Add config file option and check existance.
    Much of the file is replaced since I fixed the tabs to 4 spaces.

--- pat.py
+++ pat.py
@@ -5,30 +5,37 @@ import logging
 import os
 
 if __name__ == "__main__":
-  # Parse command line arguments
-  parser = argparse.ArgumentParser(description='Process command line options.')
-  parser.add_argument('-d','--debug', action='store_true', help='Enable debug logging')
-  parser.add_argument('--version', action='version', version='0')
-  args = parser.parse_args()
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description='Process command line options.')
+    parser.add_argument('-d','--debug', action='store_true', help='Enable debug logging')
+    parser.add_argument('-c','--config', help='Specify a config file to use',
+                        type=str, default='config.yaml')
+    parser.add_argument('--version', action='version', version='0')
+    args = parser.parse_args()
 
-  # Setup logging options
-  log_level = logging.DEBUG if args.debug else logging.INFO
-  log = logging.getLogger(os.path.basename(__file__))
-  log.setLevel(log_level)
-  formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(funcName)s(%(lineno)i):%(message)s')
+    # Setup logging options
+    log_level = logging.DEBUG if args.debug else logging.INFO
+    log = logging.getLogger(os.path.basename(__file__))
+    log.setLevel(log_level)
+    formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(funcName)s(%(lineno)i):%(message)s')
 
-  ## Console Logging
-  ch = logging.StreamHandler()
-  ch.setLevel(log_level)
-  ch.setFormatter(formatter)
-  log.addHandler(ch)
+    ## Console Logging
+    ch = logging.StreamHandler()
+    ch.setLevel(log_level)
+    ch.setFormatter(formatter)
+    log.addHandler(ch)
 
-  ## File Logging
-  fh = logging.FileHandler(os.path.basename(__file__) + '.log')
-  fh.setLevel(log_level)
-  log.addHandler(fh)
-  fh.setFormatter(formatter)
+    ## File Logging
+    fh = logging.FileHandler(os.path.basename(__file__) + '.log')
+    fh.setLevel(log_level)
+    log.addHandler(fh)
+    fh.setFormatter(formatter)
 
-  log.info("PAT Initializing...")
-  log.info("PAT Initialized")
-  log.info("PAT shutting down...")
+    log.info("PAT Initializing...")
+    if os.path.isfile(args.config):
+        log.info("Loading config file %s" % args.config)
+    else:
+        log.info("Config file does not exist, creating a default config...")
+
+    log.info("PAT Initialized")
+    log.info("PAT shutting down...")
